4c47ce08ff9cd5aa626bf2f2c6b8b5af29873128
w3programer <heshamtatawy@gmail.com>
2018-12-18 01:28:23 +0300

    18/12

--- Salonat/Models/Banks.swift
+++ Salonat/Models/Banks.swift
@@ -1,9 +1,12 @@
-//
-//  Banks.swift
-//  Salonat
-//
-//  Created by Hesham on 12/9/18.
-//  Copyright © 2018 salman. All rights reserved.
-//
-
-import Foundation
+import SwiftyJSON
+class banksModel: NSObject {
+    public var account_number : String?
+    public var account_IBAN : String?
+    public var account_bank_name : String?
+    init?(dic:[String:JSON]) {
+        guard let account_number = dic["account_number"]?.string ,let account_IBAN = dic["account_IBAN"]?.string ,let account_bank_name = dic["account_bank_name"]?.string else {return}
+        self.account_IBAN = account_IBAN
+        self.account_number = account_number
+        self.account_bank_name = account_bank_name
+    }
+}
